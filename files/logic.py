99812a6894c221711dff2c48b28590c9c99680e5
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2020-08-29 05:03:43 +0530

    console input rearrangement

--- files/logic.py
+++ files/logic.py
@@ -1,4 +1,5 @@
 import random
+from files.ConsoleInput import ConsoleInput
 
 
 class Weapon:
@@ -18,46 +19,7 @@ class Weapon:
         self.dexterity += level - 1
 
 
-class Player:
-    # def __init__(my, i):
-    #     my.name = input('enter the name of player' + str(i)+ ' => ')
-
-    #     my.health = int(input('enter the health of player' + str(i)+ ' => '))
-    #     while my.health > 100:
-    #             print("aukat me value daaliye")
-    #             my.health = int(input('enter the health of player' + str(i)+ ' => '))
-
-    #     my.agility = int(input('enter the agility of player' + str(i)+ ' => '))
-    #     while my.agility > 20:
-    #             print("aukat me value daaliye")
-    #             my.agility = int(input('enter the agility of player' + str(i)+ ' => '))
-
-    #     while my.health + my.agility > 100:
-    #         print('sum of health and agility cannot exceed 100 !')
-
-    #         my.health = int(input('enter the health of player' + str(i)+ ' => '))
-    #         while my.health > 100:
-    #             print("aukat me value daaliye")
-    #             my.health = int(input('enter the health of player' + str(i)+ ' => '))
-
-    #         my.agility = int(input('enter the agility of player' + str(i)+ ' => '))
-    #         while my.agility > 20:
-    #             print("aukat me value daaliye")
-    #             my.agility = int(input('enter the agility of player' + str(i)+ ' => '))
-
-    #     print('Choose your weapon',my.name,'(Name - Damage, Dexterity)')
-    #     j = 1
-    #     for weapon in weapons:
-    #         print(str(j) + ". " + weapon.name + " - " + str(weapon.damage) + ", " + str(weapon.dexterity))
-    #         j += 1
-
-    #     choice = int(input(' => ')) - 1
-    #     while choice < 1 and choice > len(weapons):
-    #         print('choose from the option given')
-    #         choice = int(input(' => ')) - 1
-
-    #     my.weapon = weapons[choice]
-
+class Player(ConsoleInput):
     def __init__(my, name, health, agility, weapon):
         my.name = name
         my.health = health
